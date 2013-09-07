a11dfcab06ca50d565850a08ab5f243ca2f2bb27
ArthiRengasamy <arthi.rengasamy@gmail.com>
2013-09-06 22:09:02 -0700

    make a basic user model

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,3 +1,18 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :integer          not null, primary key
+#  name       :string(255)
+#  gender     :string(255)
+#  email      :string(255)
+#  address    :text
+#  area       :string(255)
+#  other      :text
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
